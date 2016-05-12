0162e3ed6548683f70caeaedf279713d44419a84
ecanziba <elvis554@gmail.com>
2016-05-12 14:56:57 +0200

    Call Changes

--- extensions/js/chrome_tab.js
+++ extensions/js/chrome_tab.js
@@ -1,22 +1,8 @@
-/*$(function() {
-
-	//Call API From https://theysaidso.com/api/
-	var url = "https://santapanminda.com/api.php?format=json";
-	var quote = $("#quote");
-
-	$.get(url, function(data){
-
-		var new_quote = JSON.parse(data);
-
-		quote.text(new_quote.posts[0].post.content);
-
-	});
-
-});*/
+'use strict'
 
 $(function() {
 
-	//Call API From https://theysaidso.com/api/
+	//Call API From http://quotesondesign.com/
 	var url = "http://quotesondesign.com/api/3.0/api-3.0.json";
 	var quote_chrome = $("#quote");
 
